22d08e32b9caad0b225f790c8d6f9ce5c2d50171
meso272 <liujinyang1201@126.com>
2022-05-21 00:42:49 -0700

    111

--- test/qoz.cpp
+++ test/qoz.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
-#include "SZ3/api/sz.hpp"
+#include "QoZ/api/sz.hpp"
 
 
 #define SZ_FLOAT 0
@@ -35,6 +35,7 @@ void usage() {
     printf("	-f: single precision (float type)\n");
     printf("	-d: double precision (double type)\n");
     printf("	-I <width>: integer type (width = 32 or 64)\n");
+    printf("    -q: activate qoz features");
     printf("* configuration file: \n");
     printf("	-c <configuration file> : configuration file qoz.config\n");
     printf("* error control: (the error control parameters here will overwrite the setting in sz.config)\n");
@@ -52,6 +53,14 @@ void usage() {
 //    printf("		-P <point-wise relative error bound>: specifying point-wise relative error bound\n");
     printf("		-S <PSNR>: specifying PSNR\n");
     printf("		-N <normErr>: specifying normErr\n");
+    printf("    -q: activate qoz features");
+    printf("    -T <QoZ tuning target> \n");
+    printf("    tuning targets as follows: \n");
+    printf("        PSNR (peak signal-to-noise ratio)\n");
+    printf("        CR (compression ratio)\n");
+    printf("        SSIM (structural similarity)\n");
+    printf("        AC (autocorrelation)\n");
+
     printf("* dimensions: \n");
     printf("	-1 <nx> : dimension for 1D data such as data[nx]\n");
     printf("	-2 <nx> <ny> : dimensions for 2D data such as data[ny][nx]\n");
@@ -123,12 +132,12 @@ void usage_sz2() {
 }
 
 template<class T>
-void compress(char *inPath, char *cmpPath, SZ::Config conf) {
+void compress(char *inPath, char *cmpPath, QoZ::Config conf) {
     T *data = new T[conf.num];
-    SZ::readfile<T>(inPath, conf.num, data);
+    QoZ::readfile<T>(inPath, conf.num, data);
 
     size_t outSize;
-    SZ::Timer timer(true);
+    QoZ::Timer timer(true);
     char *bytes = SZ_compress<T>(conf, data, outSize);
     double compress_time = timer.stop();
 
@@ -138,7 +147,7 @@ void compress(char *inPath, char *cmpPath, SZ::Config conf) {
     } else {
         strcpy(outputFilePath, cmpPath);
     }
-    SZ::writefile(outputFilePath, bytes, outSize);
+    QoZ::writefile(outputFilePath, bytes, outSize);
 
     printf("compression ratio = %.2f \n", conf.num * 1.0 * sizeof(T) / outSize);
     printf("compression time = %f\n", compress_time);
@@ -150,13 +159,13 @@ void compress(char *inPath, char *cmpPath, SZ::Config conf) {
 
 template<class T>
 void decompress(char *inPath, char *cmpPath, char *decPath,
-                SZ::Config conf,
+                QoZ::Config conf,
                 int binaryOutput, int printCmpResults) {
 
     size_t cmpSize;
-    auto cmpData = SZ::readfile<char>(cmpPath, cmpSize);
+    auto cmpData = QoZ::readfile<char>(cmpPath, cmpSize);
 
-    SZ::Timer timer(true);
+    QoZ::Timer timer(true);
     T *decData = SZ_decompress<T>(conf, cmpData.get(), cmpSize);
     double compress_time = timer.stop();
 
@@ -167,16 +176,16 @@ void decompress(char *inPath, char *cmpPath, char *decPath,
         strcpy(outputFilePath, decPath);
     }
     if (binaryOutput == 1) {
-        SZ::writefile<T>(outputFilePath, decData, conf.num);
+        QoZ::writefile<T>(outputFilePath, decData, conf.num);
     } else {
-        SZ::writeTextFile<T>(outputFilePath, decData, conf.num);
+        QoZ::writeTextFile<T>(outputFilePath, decData, conf.num);
     }
     if (printCmpResults) {
         //compute the distortion / compression errors...
         size_t totalNbEle;
-        auto ori_data = SZ::readfile<T>(inPath, totalNbEle);
+        auto ori_data = QoZ::readfile<T>(inPath, totalNbEle);
         assert(totalNbEle == conf.num);
-        SZ::verify<T>(ori_data.get(), decData, conf.num);
+        QoZ::verify<T>(ori_data.get(), decData, conf.num);
     }
     delete[]decData;
 
@@ -204,8 +213,11 @@ int main(int argc, char *argv[]) {
     char *pwrErrorBound = nullptr;
     char *psnrErrorBound = nullptr;
     char *normErrorBound = nullptr;
+    char *tuningTarget = nullptr;
 
     bool sz2mode = false;
+    bool qoz=false;
+    bool testLorenzo=false;
 
     size_t r4 = 0;
     size_t r3 = 0;
@@ -231,7 +243,7 @@ int main(int argc, char *argv[]) {
                 usage();
                 exit(0);
             case 'v':
-                printf("version: %s\n", PROJECT_VER);
+                printf("version: %s\n", QoZ_VER);
                 exit(0);
             case 'b':
                 binaryOutput = true;
@@ -269,6 +281,7 @@ int main(int argc, char *argv[]) {
             case 'd':
                 dataType = SZ_DOUBLE;
                 break;
+
             case 'I':
                 if (++i == argc || sscanf(argv[i], "%d", &width) != 1) {
                     usage();
@@ -285,6 +298,12 @@ int main(int argc, char *argv[]) {
                 if (++i == argc)
                     usage();
                 inPath = argv[i];
+                break;
+            case 'q':
+                qoz = true;
+                break;
+            case 'l':
+                testLorenzo = true;
                 break;
             case 'o':
                 if (++i == argc)
@@ -357,6 +376,12 @@ int main(int argc, char *argv[]) {
                     usage();
                 psnrErrorBound = argv[i];
                 break;
+
+            case 'T':
+                if (++i == argc)
+                    usage();
+                tuningTarget = argv[i];
+                break;
             default:
                 usage();
                 break;
@@ -391,18 +416,24 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
-    SZ::Config conf;
+    QoZ::Config conf;
     if (r2 == 0) {
-        conf = SZ::Config(r1);
+        conf = QoZ::Config(r1);
     } else if (r3 == 0) {
-        conf = SZ::Config(r2, r1);
+        conf = QoZ::Config(r2, r1);
     } else if (r4 == 0) {
-        conf = SZ::Config(r3, r2, r1);
+        conf = QoZ::Config(r3, r2, r1);
     } else {
-        conf = SZ::Config(r4, r3, r2, r1);
+        conf = QoZ::Config(r4, r3, r2, r1);
     }
     if (compression && conPath != nullptr) {
         conf.loadcfg(conPath);
+    }
+    if (qoz){
+        conf.QoZ=1;
+    }
+    if (testLorenzo){
+        conf.testLorenzo=1;
     }
 
     if (errBoundMode != nullptr) {
@@ -421,36 +452,61 @@ int main(int argc, char *argv[]) {
                 conf.l2normErrorBound = atof(normErrorBound);
             }
         }
-        if (strcmp(errBoundMode, SZ::EB_STR[SZ::EB_ABS]) == 0) {
-            conf.errorBoundMode = SZ::EB_ABS;
+        if (strcmp(errBoundMode, QoZ::EB_STR[QoZ::EB_ABS]) == 0) {
+            conf.errorBoundMode = QoZ::EB_ABS;
             if (errBound != nullptr) {
                 conf.absErrorBound = atof(errBound);
             }
-        } else if (strcmp(errBoundMode, SZ::EB_STR[SZ::EB_REL]) == 0 || strcmp(errBoundMode, "VR_REL") == 0) {
-            conf.errorBoundMode = SZ::EB_REL;
+        } else if (strcmp(errBoundMode, QoZ::EB_STR[QoZ::EB_REL]) == 0 || strcmp(errBoundMode, "VR_REL") == 0) {
+            conf.errorBoundMode = QoZ::EB_REL;
             if (errBound != nullptr) {
                 conf.relErrorBound = atof(errBound);
             }
-        } else if (strcmp(errBoundMode, SZ::EB_STR[SZ::EB_PSNR]) == 0) {
-            conf.errorBoundMode = SZ::EB_PSNR;
+        } else if (strcmp(errBoundMode, QoZ::EB_STR[QoZ::EB_PSNR]) == 0) {
+            conf.errorBoundMode = QoZ::EB_PSNR;
             if (errBound != nullptr) {
                 conf.psnrErrorBound = atof(errBound);
             }
-        } else if (strcmp(errBoundMode, SZ::EB_STR[SZ::EB_L2NORM]) == 0) {
-            conf.errorBoundMode = SZ::EB_L2NORM;
+        } else if (strcmp(errBoundMode, QoZ::EB_STR[QoZ::EB_L2NORM]) == 0) {
+            conf.errorBoundMode = QoZ::EB_L2NORM;
             if (errBound != nullptr) {
                 conf.l2normErrorBound = atof(errBound);
             }
-        } else if (strcmp(errBoundMode, SZ::EB_STR[SZ::EB_ABS_AND_REL]) == 0) {
-            conf.errorBoundMode = SZ::EB_ABS_AND_REL;
-        } else if (strcmp(errBoundMode, SZ::EB_STR[SZ::EB_ABS_OR_REL]) == 0) {
-            conf.errorBoundMode = SZ::EB_ABS_OR_REL;
+        } else if (strcmp(errBoundMode, QoZ::EB_STR[QoZ::EB_ABS_AND_REL]) == 0) {
+            conf.errorBoundMode = QoZ::EB_ABS_AND_REL;
+        } else if (strcmp(errBoundMode, QoZ::EB_STR[QoZ::EB_ABS_OR_REL]) == 0) {
+            conf.errorBoundMode = QoZ::EB_ABS_OR_REL;
         } else {
             printf("Error: wrong error bound mode setting by using the option '-M'\n");
             usage();
             exit(0);
         }
     }
+
+    if (tuningTarget!= nullptr) {
+       
+        if (strcmp(tuningTarget, "PSNR") == 0) {
+            conf.tuningTarget = QoZ::TUNING_TARGET_RD;
+        }
+        else if (strcmp(tuningTarget, "CR") == 0) {
+            conf.tuningTarget = QoZ::TUNING_TARGET_CR;
+        }
+        else if (strcmp(tuningTarget, "SSIM") == 0) {
+            conf.tuningTarget = QoZ::TUNING_TARGET_SSIM;
+        }
+        else if (strcmp(tuningTarget, "AC") == 0) {
+            conf.tuningTarget = QoZ::TUNING_TARGET_AC;
+        }
+        else {
+            printf("Error: wrong tuning target setting by using the option '-T'\n");
+            usage();
+            exit(0);
+        }
+        
+    }
+
+
+
 
     if (compression) {
 
